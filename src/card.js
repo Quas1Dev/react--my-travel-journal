0f188b93c30a767952edbaf5377af324b879778d
Fernando Bonfim <devnoob@vivaldi.net>
2022-07-30 08:48:49 -0300

    Added classes to Card component; styled Card component

--- src/card.js
+++ src/card.js
@@ -2,14 +2,16 @@ import pointer from "./images/pointer.png"
 export default function Card(props) {
   return (
     <section className="guide--card">
-      <span>
-        <img src={pointer}/>
-        {props.country.toUpperCase()}
-      </span>
-      <img src={props.coverImg}/>
-      <h2>{props.title}</h2>
-      <span className="card--date_interval">{props.tripInterval}</span>
-      <p>{props.siteDescription}</p>
+      <img src={props.coverImg} className="card--cover_img"/>
+      <div className="card--body">
+        <span className="card--location">
+          <img src={pointer}/>
+          {props.country.toUpperCase()}
+        </span>
+        <h2 className="card--title">{props.title}</h2>
+        <span className="card--date_interval">{props.tripInterval}</span>
+        <p className="card--description">{props.siteDescription}</p>
+      </div>
     </section>
   )
 }
