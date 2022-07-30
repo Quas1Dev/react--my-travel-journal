7970c505f9fb0771a62edb93c7ca15f27ab5e609
Fernando Bonfim <devnoob@vivaldi.net>
2022-07-30 06:58:17 -0300

    Added header component.

---
+++ src/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header id="pg-header">
+      <img src={logo} alt="A projection of Earth on a circle."/>
+      <h1>my travel journal</h1>
+    </header>
+  )
+}
